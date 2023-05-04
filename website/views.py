e7211c4770db1e83a5335ae9bd29be87218b58ab
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2023-05-04 19:15:26 +0300

    Rendering the index

---
+++ website/views.py
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("index.html")
+>>>>>>> Stashed changes
