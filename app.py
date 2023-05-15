dd83ca566fb36220ae8924b0a264d31962b00728
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2023-05-15 23:32:07 +0300

    The header and p text working

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import pandas as pd
 from dash import Dash, dcc, html
+import plotly.express as px
 
-df = pd.read_csv("pokemondb.csv")
-print(df)
-    
+data = pd.read_csv("pokemondb.csv")
 
 app = Dash(__name__)
+app.layout = html.Div(
+    children=[
+        html.H1(children="Pokemon analytics"),
+        html.P(children=
+            "tadaa"
+        ),
+    ]
+)
+
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
