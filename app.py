4c2f995a5b16a780bf47bc2888f427bf71e049f4
Veera Määttänen <38630359+veerux@users.noreply.github.com>
2023-05-22 18:02:22 +0300

    changed a lot of things, scatter plot not good for this so needs a change still

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 import pandas as pd
-from dash import Dash, dcc, html
 import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 
-data = pd.read_csv("pokemondb.csv")
+# Load the data from the CSV file
 
-app = Dash(__name__)
-app.layout = html.Div(
-    children=[
-        html.H1(children="Pokemon analytics"),
-        html.P(children=
-            "tadaa"
-        ),
-    ]
+df = pd.read_csv("pokemondb.csv")
+
+# Create the Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Pokemon Scatter Plot"),
+    html.Div([
+        html.Label("Type 1:"),
+        dcc.Dropdown(
+            id='type1-dropdown',
+            options=[{'label': t, 'value': t} for t in df['Type 1'].unique()],
+            value='Grass'
+        )
+    ]),
+    dcc.Graph(id='scatter-plot')
+])
+
+# Define the callback function to update the scatter plot based on the dropdown selection
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    [Input('type1-dropdown', 'value')]
 )
+def update_scatter_plot(type1):
+    filtered_df = df[df['Type 1'] == type1]
+    fig = px.scatter(filtered_df, x='Type 1', y='Total', color='Legendary', title=f"Scatter Plot for {type1}")
+    return fig
 
-
-
-if __name__ == "__main__":
+# Run the app
+if __name__ == '__main__':
     app.run_server(debug=True)
